815a5b889fbe06f586ef696f0bace74d9fe242f8
cidy <cidywallace12@gmail.com>
2025-09-12 17:08:51 -0300

    first commit

---
+++ src/main/java/ufpb/project/acervodigital/AcervoDigitalApplication.java
@@ -0,0 +1,13 @@
+package ufpb.project.acervodigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcervoDigitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcervoDigitalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ufpb/project/acervodigital/AcervoDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package ufpb.project.acervodigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcervoDigitalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
